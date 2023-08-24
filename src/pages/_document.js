8f0c2dca7a7eed3d9df186c4288083cd54ef437a
S. Sifantus <hi@ssifantus.com>
2023-08-24 15:38:27 -0700

    remove duplicate script tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, {Head, Html, Main, NextScript} from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -23,7 +22,6 @@ class MyDocument extends Document {
         <body>
         <Main/>
         <NextScript/>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-53978CWP5N"></Script>
         </body>
       </Html>
     );
