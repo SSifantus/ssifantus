b19a8ef59cf3fd84d70a01e968c569159a2a894a
S. Sifantus <hi@ssifantus.com>
2023-08-24 18:10:21 -0700

    add fallback page

---
+++ src/pages/work.jsx
@@ -0,0 +1,30 @@
+import Layouts from "@layouts/Layouts";
+import dynamic from "next/dynamic";
+
+import { getSortedProjectsData } from "@library/interactive";
+
+import PageBanner from "@components/PageBanner";
+
+const ProjectsGrid = dynamic( () => import("@components/ProjectsGrid"), { ssr: false } );
+
+const Portfolio = (props) => {
+  return (
+    <Layouts>
+      <PageBanner pageTitle={"Work"} />
+
+      <ProjectsGrid projects={props.projects} cols={"3"} layout={"grid"} masonry />
+
+    </Layouts>
+  );
+};
+export default Portfolio;
+
+export async function getStaticProps() {
+  const allProjects = getSortedProjectsData();
+
+  return {
+    props: {
+      projects: allProjects
+    }
+  }
+}
